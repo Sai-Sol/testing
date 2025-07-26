2e5c071692ed77f4286fd64c26cdd80171028346
S J Sakthivel Raja <sraja126@gmail.com>
2025-07-26 09:19:56 +0000

    Initial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRpvZkqm-Ksc6n8lFkbA_LzK4vkUnaLIY",
+  authDomain: "quantumchain-logger.firebaseapp.com",
+  projectId: "quantumchain-logger",
+  storageBucket: "quantumchain-logger.appspot.com",
+  messagingSenderId: "231859771147",
+  appId: "1:231859771147:web:a03c274726a495feb1b40e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
